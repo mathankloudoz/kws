9c8a667809dc006df8f40676dade19100f49aedb
mathankloudoz <130228159+mathankloudoz@users.noreply.github.com>
2023-04-19 18:31:32 +0530

    Add files via upload

---
+++ lib/model/home_card.dart
@@ -0,0 +1,17 @@
+class HomeCard {
+  final String id;
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+  final String stock;
+  final double value;
+
+  HomeCard({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+    required this.subTitle,
+    required this.stock,
+    required this.value,
+  });
+}
