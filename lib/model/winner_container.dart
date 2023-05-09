8dce482df07b831422aa643b579db4c132b8115a
mathankloudoz <130228159+mathankloudoz@users.noreply.github.com>
2023-05-09 18:39:09 +0530

    Add files via upload

---
+++ lib/model/winner_container.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class WinnerContainerModel {
+  final String id;
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+  final String subTitle1;
+  final String subtitle2;
+  WinnerContainerModel({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+    required this.subTitle,
+    required this.subTitle1,
+    required this.subtitle2,
+  });
+}
