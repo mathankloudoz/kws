f647b9f84b78b93055707080192f4919e0682213
mathankloudoz <130228159+mathankloudoz@users.noreply.github.com>
2023-05-12 18:37:11 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kws/views/screens/Home_screen.dart';
+import 'package:kws/views/screens/bottom_navigator/bottom_navigator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KWS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BottomNavigator(),
+    );
+  }
+}
---
+++ lib/model/notification_model.dart
@@ -0,0 +1,10 @@
+class notificaitonPage {
+  String title;
+  String subtitle;
+  String description;
+  String imageUrl;
+  String data;
+
+  notificaitonPage(
+      this.title, this.subtitle, this.description, this.imageUrl, this.data);
+}
